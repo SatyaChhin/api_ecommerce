f2bfb907c2639e4f0e35fcde0d43a40fe33d4ed9
NonameTest <nonametest.no1@gmail.com>
2023-08-18 12:05:22 +0700

    implement swagger to project

--- routes/api/banner.js
+++ routes/api/banner.js
@@ -4,6 +4,51 @@ const routes = express.Router()
 const multer = require('multer')
 const path = require('path')
 
+// this define the banner schemas
+/** 
+ * @swagger
+ * components:
+ *  schemas:
+ *      Banner:
+ *          type: object
+ *          required:
+ *              - image
+ *              - description
+ *          properties:
+ *              banner_id:
+ *                  type: Integer
+ *                  description: auto increment id
+ *              image:
+ *                  type: String
+ *                  description: image path
+ *              description:
+ *                  type: String    
+ *                  description: description of the banner
+ *              created_at:
+ *                  type: Date
+ *                  description: create date of banner
+ *              updated_at:
+ *                  type: Date
+ *                  description: update date of banner
+ */
+
+/**
+ * @swagger
+ * /banner:
+ *  get:
+ *      summary: Return the list of all banners ORDER BY banner_id  DESC
+ *      responses:
+ *          200:
+ *              description: list of banners
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/Banner'
+ * 
+ */
+
 const storage = multer.diskStorage({
     destination : './public/upload/image',
     filename: ( req , file , callBack )=>{
--- server.js
+++ server.js
@@ -1,6 +1,34 @@
 const express = require("express")
-const app = express()
 const bodyParser = require('body-parser')
+//swagger
+const swaggerUI = require("swagger-ui-express")
+const swaggerJsDoc = require("swagger-jsdoc")
+
+const definition = {
+}
+
+const options = {
+    definition:{
+        openapi: "3.0.0",
+        info: {
+            title: "API_ECOMMERCE",
+            version: "1.0.0",
+            description: ""
+        },
+        servers: [
+            {
+                url: "http://localhost:8000"
+            }
+        ]
+    },
+    // this will locate the route
+    apis:["./routes/api/*.js"]
+}
+
+const specs = swaggerJsDoc(options);
+const app = express()
+//swagger doc route
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
 // parse application/json
 app.use(bodyParser.json())
 // parse application x-www-form-urlencoded
@@ -18,10 +46,10 @@ app.use(require("./routes/api/banner"))
 app.use(require("./routes/api/wishlist"))
 app.use(require("./routes/api/customer"))
 
-
 const port = process.env.PORT || 8000
-app.listen(port , () => {
+app.listen(port, () => {
     console.log('Server started at http://localhost:' + port);
+    console.log('=> Server swagger doc at http://localhost:' + port + "/api-docs")
 })
 
 
